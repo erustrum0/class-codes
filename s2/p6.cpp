792dd7e4a1eb3cd2c1bdb259076952b758902cbb
erustrum0 <157773321+erustrum0@users.noreply.github.com>
2024-01-31 04:37:37 +0000

    Fixed volume calculator.

--- s2/p6.cpp
+++ s2/p6.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 
 int main() {
-    //V = 4/3*pi*r*r*r - Formula for volume
+    //V = (4*M_PI*(r*r*r))/3 - Formula for volume
     double r;
     cout << "Enter a radius (inches): ";
     cin >> r;
-    double V = 4/3*M_PI*r*r*r;
+    double V = (4*M_PI*(r*r*r))/3;
     cout << "The volume of the described sphere is " << V << " cubic inches.\n";
 }
