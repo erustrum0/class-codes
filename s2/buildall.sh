0383f80cba4be7411f84207effdbe86088457c46
erustrum0 <157773321+erustrum0@users.noreply.github.com>
2024-02-02 03:10:49 +0000

    Finished the whole thing!

--- s2/buildall.sh
+++ s2/buildall.sh
@@ -1,3 +1,5 @@
+# this script was responsible for wiping all of my progress, but somehow i got it fixed
+# this script, when run, will compile all of the programs and output them to p*.cpp.bin
 for f in *.cpp
 do
  echo "Compiling $f..."
