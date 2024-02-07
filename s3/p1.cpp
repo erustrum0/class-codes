7de3aa0ebbca906e4d0eaf6be4f61a404a9e0ef9
erustrum0 <157773321+erustrum0@users.noreply.github.com>
2024-02-07 04:51:04 +0000

    Added the first If/Else program.

---
+++ s3/p1.cpp
@@ -0,0 +1,16 @@
+//Determines whether or not a given number is larger than 10.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    string result = "greater or equal to ";
+
+    if (number < 10) {
+        result = "less than ";
+    };
+    cout << "That number is " << result << "ten!\n";
+}
