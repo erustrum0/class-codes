5d4cbbbc41c54446b4565f3b8c6a4dab896be841
erustrum0 <157773321+erustrum0@users.noreply.github.com>
2024-02-08 00:17:49 +0000

    Finshed program 5.

--- s3/p5.cpp
+++ s3/p5.cpp
@@ -3,5 +3,27 @@
 #include <iostream>
 
 int main() {
+    int a;
+    int b;
+    int c;
+    int largest;
+
+    std::cout << "Please input three numbers:\nA: ";
+    std::cin >> a;
+    std::cout << "B: ";
+    std::cin >> b;
+    std::cout << "C: ";
+    std::cin >> c;
+
+    //We now have all the numbers. Time to sort!
+    if (a >= b && a >= c) {
+        largest = a;
+    } else if (b >= a && b >= c) {
+        largest = b;
+    } else {
+        largest = c;
+    }
+
+    std::cout << "\nThe largest is " << largest << ".\n";
     return 0;
 }
